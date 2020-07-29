8abb0289b23af738a28f060a8a02ad59288735a4
Ivan Shcherbakov <isprogfun@gmail.com>
2020-07-29 10:55:10 +0200

    Removed GitHub Pages

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
     entry: './app/index.js',
 
     output: {
-        path: path.resolve(__dirname + '/docs'),
+        path: path.resolve(__dirname + '/dist'),
         filename: 'bundle.js'
     },
 
